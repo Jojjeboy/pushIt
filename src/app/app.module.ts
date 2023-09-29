ee063a7007d59b2d0a84598232233e80e313ac1f
Johannes Kärnstam <johka96@vgregion.se>
2023-09-29 17:09:25 +0200

    Color fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './shared/modules/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -15,7 +13,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    ClarityModule,
     SharedModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
