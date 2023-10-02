621306f0de3fd42efb6fcc00ba0b9cec467da46c
Johannes Kärnstam <johka96@vgregion.se>
2023-10-02 17:18:28 +0200

    Archive modal and typos

--- src/app/shared/components/base-tally/base-tally.component.ts
+++ src/app/shared/components/base-tally/base-tally.component.ts
@@ -12,6 +12,7 @@ export class BaseTallyComponent {
 
   constructor(protected tallyService: TallyService){}
   percentage = 0.00;
+  archiveTallyModalData: Object = {};
 
   public translation: any = {
     "daily": "dagars",
@@ -48,7 +49,25 @@ export class BaseTallyComponent {
 
   increase(tally: Tally) {
     this.tallyService.increase(tally);
+
+    // Ask if it should be deactivated if its not a auto reset when the goal is hit.
+    if(tally.getValue() >= tally.getGoal() && !tally.getCanReset()){
+      console.log('Deactivate this tally?');
+      this.archiveTallyModalData = {
+        open: true,
+        header: 'Arkivera räknare?',
+        body: 'Du har uppnåt målet och räknaren nollställs inte periodvis.\n Vill du arkivera räknaren?',
+        footer: ''
+      }
+    }
     this.recalculatePercentage(tally);
+  }
+
+  archiveTallyConfirmed(tally: Tally): void {
+    // put logic here to archive 
+    this.tallyService.archive(tally);
+    this.archiveTallyModalData = { open: false };
+    console.log('Tally archived');
   }
   
   decrease(tally: Tally) {
