2c615492d3013cee8558d467e545d969c6332c8d
Johannes Kärnstam <johka96@vgregion.se>
2023-10-24 09:44:51 +0200

    Http request GET and internet connection status

--- src/app/backup-restore/backup-restore.component.ts
+++ src/app/backup-restore/backup-restore.component.ts
@@ -1,17 +1,39 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { fromEvent, merge, of, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-backup',
   templateUrl: './backup-restore.component.html',
   styleUrls: ['./backup-restore.component.scss']
 })
-export class BackupRestoreComponent {
+export class BackupRestoreComponent implements OnInit, OnDestroy {
 
 
+  networkStatus: boolean = false;
+  networkStatus$: Subscription = Subscription.EMPTY;
   lastBackupFetched: boolean = false;
   backupTalliesModalData: Object = {};
   restoreTalliesModalData: Object = {};
+  private API_URL: string = 'http://20232024.se/api/pushIt';
+  data = {};
 
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    // check if were online
+    this.checkNetworkStatus();
+    if(this.networkStatus){
+      this.getDataFromServer();
+    }
+  }
+
+
+  getDataFromServer() {
+    return this.httpClient.get(this.API_URL).subscribe((data: any) => this.data = { ...data });
+  }
 
   backup(): void {
     this.backupTalliesModalData = {
@@ -30,17 +52,37 @@ export class BackupRestoreComponent {
   }
 
   backupTalliesConfirmed(): void {
-    
+
     this.backupTalliesModalData = { open: false };
     console.log('make http PUT request');
-     
-   }
+
+  }
 
 
-   restoreTalliesConfirmed(): void {
-    
-     console.log('make http GET request');
-     this.restoreTalliesModalData = { open: false };
+  restoreTalliesConfirmed(): void {
 
-   }
+    console.log('make http GET request');
+    this.restoreTalliesModalData = { open: false };
+
+  }
+
+  checkNetworkStatus() {
+    this.networkStatus = navigator.onLine;
+    this.networkStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    )
+      .pipe(map(() => navigator.onLine))
+      .subscribe(status => {
+        this.networkStatus = status;
+      });
+  }
+
+
+  ngOnDestroy(): void {
+    this.networkStatus$.unsubscribe();
+  }
+
 }
+
