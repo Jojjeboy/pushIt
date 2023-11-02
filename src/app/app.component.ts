48d090bf29f3c108d1e18f093cf69a7a887138f5
Johannes Kärnstam <johka96@vgregion.se>
2023-11-02 16:52:16 +0100

    Test Service worker code 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,7 +93,9 @@ export class AppComponent implements OnInit {
 
       this.showAlert = this.isNewAppVersionAvailable;
       this.alertText = 'Ny version av appen finns, ladda om';
-      console.log(this.alertText);
+      if(this.showAlert){
+        console.log(this.alertText);
+      }
     })
   }
 
