cbb51eed11b7de7b316f83c2bcb4961ea31e5909
Johannes Kärnstam <johka96@vgregion.se>
2024-03-27 21:01:45 +0100

    Alert css fixed

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -3,12 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'alert',
   template: `<div class="alert alert-app-level alert-{{ type }}" *ngIf="showalert">
+  
   <div class="alert-items">
     <div class="alert-item static">
-      <div class="alert-icon-wrapper">
-      <span class="material-symbols-outlined" *ngIf="type == 'success'">check</span>
-      <span class="material-symbols-outlined" *ngIf="type == 'error'">error</span>
-      </div>
       <div class="alert-text">
         <span class="text">{{ text }}</span>
       </div>
